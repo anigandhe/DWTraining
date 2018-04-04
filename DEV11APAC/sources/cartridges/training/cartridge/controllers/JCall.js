7800d5f7fdd1aaff18b10ed8c6c18fef467caa2d
agandhe <anirudha.gandhe@gmail.com>
2018-04-04 16:48:37 -0400

    First Commit

---
+++ DEV11APAC/sources/cartridges/training/cartridge/controllers/JCall.js
@@ -0,0 +1,31 @@
+/**
+* Description of the Controller and the logic it provides
+*
+* @module  controllers/JCall
+*/
+
+var guard = require('storefront_controllers/cartridge/scripts/guard');
+var ISML = require('dw/template/ISML');
+
+function start() {
+
+	var myParam = request.httpParameterMap.param;
+	
+	if (myParam.stringValue != null)
+	{
+		ISML.renderTemplate(
+				'call/jnotEmpty.isml', {paramOnPdict:myParam}
+				);
+	}
+	else
+	{
+		ISML.renderTemplate(
+				'call/jempty.isml', {paramOnPdict:'param not found'}
+				);
+	}
+	
+	/* response.getWriter().println(myParam.stringValue); */
+
+};
+exports.Start = guard.ensure(['get'], start);
+
